add8fb826e3cd5eb137589c5f24747ca6161f572
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-04-25 17:49:00 +0200

    Implemented formatting for functions

--- src/formatters/function.js
+++ src/formatters/function.js
@@ -1,6 +1,10 @@
 import { represent } from '../common'
 
 export function formatFunction(fmt) {
-    // TODO: implement
+    if (this.name) {
+        fmt.write('<function ', this.name, '>')
+    } else {
+        fmt.write('<function>')
+    }
 }
 Function.prototype[represent] = formatFunction
