a5db5ec9b14ef9e69b952cf1446076509c6d6066
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-03-06 13:42:51 +0100

    Object can have a null prototype

--- src/formatters/object.js
+++ src/formatters/object.js
@@ -2,8 +2,9 @@ import util from '../util'
 import { represent } from '../common'
 
 export function formatObject(fmt) {
-    const { constructor } = Reflect.getPrototypeOf(this)
-    const name = constructor === Object ? null : constructor.name
+    const prototype = Reflect.getPrototypeOf(this)
+    const constructor = prototype && prototype.constructor
+    const name = constructor === Object || !constructor ? null : constructor.name
 
     fmt.struct(name, fmt => {
         for (const key of Reflect.ownKeys(this).sort(util.compareKeys)) {
