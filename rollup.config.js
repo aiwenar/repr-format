beb0057d39a7196f2633d74ad4c04173533c055b
Krzysztof Mędrzycki <aiwenari@gmail.com>
2020-07-14 13:47:51 +0200

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import buble from 'rollup-plugin-buble'
-import resolve from 'rollup-plugin-node-resolve'
+import buble from '@rollup/plugin-buble'
+import resolve from '@rollup/plugin-node-resolve'
 
 export default {
     input: 'src/index.js',
